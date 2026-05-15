ba6e4f0f2db4fefa51b93773760f42c4b5397849
Mohidul <islamohidul856mi647360@gmail.com>
2026-05-15 13:37:16 +0600

    keep the previous url or admin

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include("toy_api.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
